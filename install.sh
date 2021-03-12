ebccf77ac945da2f26d584957f5efd32caf3979e
Maksim <56597453+zm34097@users.noreply.github.com>
2021-03-12 23:56:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,4 +21,16 @@ genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt <<- EOF
 ln -sf /usr/share/zoneinfo/Europe/Minsk /etc/localtime
 hwclock --systohc
+touch /etc/locale.conf
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+touch /etc/hostname
+echo "archlinux" >> /etc/hostname
+echo "127.0.0.1	localhost" >> /etc/hosts
+echo "::1		localhost" >> /etc/hosts
+echo "127.0.1.1	archlinux.localdomain	archlinux" >> /etc/hosts
+mkinitcpio -P
+(
+echo 1234;
+echo 1234;
+) | passwd
 EOF
