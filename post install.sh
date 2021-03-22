d130c1145d72fc1c4a415ff5682675c96b3ead06
Maksim <56597453+zm34097@users.noreply.github.com>
2021-03-22 22:01:41 +0300

    Create post install.sh

---
+++ post install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+touch /etc/systemd/network/20-wired.network
+echo "[Match]" >> /etc/systemd/network/20-wired.network 
+echo "Name=enp0s3" >> /etc/systemd/network/20-wired.network
+echo "" >> /etc/systemd/network/20-wired.network
+echo "[Network]" >> /etc/systemd/network/20-wired.network
+echo "DHCP=yes" >> /etc/systemd/network/20-wired.network
+#echo "Address=10.1.10.9/24" >> /etc/systemd/network/20-wired.network
+#echo "Gateway=10.1.10.1" >> /etc/systemd/network/20-wired.network
+#echo "DNS=10.1.10.1" >> /etc/systemd/network/20-wired.network
+
+
